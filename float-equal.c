d1b30ffd9d1f5025be129e45ec70caa2c0a38aff
liuchang404 <1016057109@qq.com>
2019-10-21 20:18:02 +0800

    helloworld add commit

--- float-equal.c
+++ float-equal.c
@@ -6,7 +6,7 @@ int main()
     float m = 1.78;
     float x = 1.779999, y = 2;
     float z = m / y, z1 = x / y;
-    if (z == z1) {
+    if (fabs(z-z1)<0.00001) {
         z = (m + x) / y;
     }
     printf("Z is %f\n", z);
